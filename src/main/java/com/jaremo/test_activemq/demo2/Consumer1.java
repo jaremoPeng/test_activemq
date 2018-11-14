0a89dbdd87a9e5e70fcdcaffddb84155c401baf1
jaremo <jaremo@163.com>
2018-11-14 20:08:46 +0800

    消息中间件的两种模式

---
+++ src/main/java/com/jaremo/test_activemq/demo2/Consumer1.java
@@ -0,0 +1,37 @@
+package com.jaremo.test_activemq.demo2;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+
+import javax.jms.*;
+
+/**
+ * 消息接受者（订阅发布者模式）
+ */
+public class Consumer1 {
+
+    public static void main(String[] args) throws JMSException {
+        // 创建连接工厂
+        ConnectionFactory connectionFactory = new ActiveMQConnectionFactory("tcp://127.0.0.1:61616");
+        // 创建连接
+        Connection connection = connectionFactory.createConnection();
+        // 需要一个标识订阅者的id（持久化）
+        connection.setClientID("tp_cli2");
+        // 开启连接
+        connection.start();
+        // 创建会话 提供两个参数 第一个参数为true 代表支持事务
+        Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+        // 创建一个消息主题
+        Topic topic = session.createTopic("mq_topic");
+        // 构建消息接收者
+        MessageConsumer consumer = session.createDurableSubscriber(topic,"cli_name2"); // 订阅的主题，订阅者的名字
+        // 接受消息
+        TextMessage receive = (TextMessage)consumer.receive();
+        // 通过消息发送者来的消息
+        String text = receive.getText();
+        System.out.println("订阅者2 接受到的消息为 "+text);
+        // 关闭的操作
+        consumer.close();
+        session.close();
+        connection.close();
+    }
+}
--- src/main/java/com/jaremo/test_activemq/demo2/Producer.java
+++ src/main/java/com/jaremo/test_activemq/demo2/Producer.java
@@ -1,11 +1,11 @@
-package com.jaremo.test_activemq.demo1;
+package com.jaremo.test_activemq.demo2;
 
 import org.apache.activemq.ActiveMQConnectionFactory;
 
 import javax.jms.*;
 
 /**
- * 消息发送者（点对点模式（p2p） 不要求消息接受者一直在线，消息只能被一个消息接受者处理）
+ * 消息发送者（订阅发布者模式： 多个订阅者订阅同个主题topic， 在所有订阅者中有不同的标识（id），不同名称）
  */
 public class Producer {
 
@@ -18,10 +18,10 @@ public class Producer {
         connection.start();
         // 创建会话 提供两个参数 第一个参数为true 代表支持事务
         Session session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
-        // 创建一个消息队列
-        Queue queue = session.createQueue("mq_first");
+        // 创建一个消息主题
+        Topic topic = session.createTopic("mq_topic");
         // 构建消息发送者
-        MessageProducer producer = session.createProducer(queue);
+        MessageProducer producer = session.createProducer(topic);
         // 添加消息
         TextMessage msg = session.createTextMessage("Hello I am boss");
         // 通过消息发送者来发送消息
